49208c6411071252ab0adcc6d251542d49e5a698
beetleman <mateusz.probachta@gmail.com>
2015-06-17 10:47:53 +0200

    Add setup scrtipt for fedora>=22.

---
+++ setup_i3_fedors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+dnf install -y i3 i3lock i3status dmenu dunst lxpolkit xfce4-power-manager \
+    pulseaudio-module-x11 xorg-x11-xkb-utils parcellite pidgin thunderbird \
+    firewall-applet volumeicon network-manager-applet emacs stjerm feh
+
