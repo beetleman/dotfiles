c93571ad56858526ca4b2f782b4f84bdd261351b
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-11-05 10:18:35 +0100

    add command to install all configs

---
+++ install_configs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+function install_conf {
+    rm ~/$2 -rf
+    ln -s `pwd`/$1/$2 ~/
+}
+
+install_conf i3 .i3
+install_conf Xresources .Xresources
+
+# install oh-my-shell
+install_conf zsh .oh-my-zsh
+install_conf zsh .zshrc
