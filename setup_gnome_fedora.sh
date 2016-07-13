39d60c2a8b9f26f0e797e6c02bd7a09c72e3539f
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-07-13 12:52:22 +0200

    update install scripts

--- setup_gnome_fedora.sh
+++ setup_gnome_fedora.sh
@@ -2,21 +2,26 @@
 
 dnf update -y
 
+# install additional repos and feedy
+curl http://folkswithhats.org/fedy-installer -o fedy-installer && \
+    chmod +x fedy-installer && \
+    ./fedy-installer
+
+
+# install apps
 dnf install -y emacs gnome-tweak-tool emacs firefox thunderbird gimp \
-    gpick zile mc smplayer vlc
+    gpick zile mc smplayer vlc tmux
 
-dnf install -y git docker-compose docker python-devel python3-devel sbcl clisp
-
-dnf install -y sqlite-devel zlib-devel tcl-devel tk-devel postgresql-devel
+# install programing stuff
+dnf install -y git docker-compose docker python-devel python3-devel sbcl clisp zsh
 
 dnf group install -y \
     "C Development Tools and Libraries" \
     "Development Tools" \
     "RPM Development Tools"
 
-curl http://folkswithhats.org/fedy-installer -o fedy-installer && \
-    chmod +x fedy-installer && \
-    ./fedy-installer
+# install libs
+dnf install -y sqlite-devel zlib-devel tcl-devel tk-devel postgresql-devel
 
-
+# install configs
 bash ./install_configs.sh
--- setup_i3_fedors.sh
+++ setup_i3_fedors.sh
@@ -2,5 +2,4 @@
 
 dnf install -y i3 i3lock i3status dmenu dunst lxpolkit xfce4-power-manager \
     pulseaudio-module-x11 xorg-x11-xkb-utils parcellite pidgin thunderbird \
-    firewall-applet volumeicon network-manager-applet emacs stjerm feh
-
+    firewall-applet volumeicon network-manager-applet emacs guake feh
