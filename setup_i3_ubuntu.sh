c45f8bc749478f08527b991b71519bdb10217988
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-10-23 14:49:49 +0200

    ubuntu configs

---
+++ setup_i3_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get install -y i3 i3lock i3status suckless-tools dunst lxpolkit xfce4-power-manager \
+    pulseaudio-module-x11 x11-xkb-utils parcellite thunderbird \
+    volumeicon-alsa network-manager-gnome guake feh rxvt-unicode-256color \
+    lxappearance xfonts-terminus light-locker
