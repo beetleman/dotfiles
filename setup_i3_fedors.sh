992a2677117d39cb39a5c9b7e48c4e2336aa98e9
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-05-31 23:06:11 +0200

    update i3 config

--- setup_i3_fedors.sh
+++ setup_i3_fedors.sh
@@ -3,13 +3,15 @@
 bash ./setup_common_fedora.sh
 
 sudo dnf install -y i3 i3lock i3status dmenu dunst lxpolkit \
-     xfce4-power-manager light-locker \
+     xfce4-power-manager \
      pulseaudio-module-x11 xorg-x11-xkb-utils parcellite  \
      firewall-applet volumeicon network-manager-applet guake feh blueman\
      thunar scrot rxvt-unicode-256color \
      lightdm-gtk-greeter-settings lightdm-gtk lightdm \
      numix-gtk-theme numix-icon-theme \
      system-switch-displaymanager arandr
+
+xfconf-query -c xfce4-session -p /general/LockCommand -s "i3lock" --create -t string
 
 sudo dnf copr enable yaroslav/i3desktop
 sudo dnf install rofi -y
